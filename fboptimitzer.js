8d337685e69590f7c1ff8366fb33aa3bb5d04f14
Dominic Diehn <Dominic Diehn>
2017-02-13 20:24:25 -0600

    deletes of clickhole on ctrl enter

--- fboptimitzer.js
+++ fboptimitzer.js
@@ -1,1 +1,19 @@
-$("body").append("<p>TestTestTestTestTestTestTestTestTestTestTestTestTeststTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest</p>");
+// $(document).keypress(function(e) {
+//   if(e.which == 13) {
+//
+//   }
+// });
+
+// http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-in-jquery/10655316#10655316
+var down = [];
+$(document).keydown(function(e) {
+    down[e.keyCode] = true;
+}).keyup(function(e) {
+    if (down[17] && down[13]) {
+      console.log('Enter was pressed');
+      // $('._4ikz').remove()
+      $('a:contains("ClickHole")').closest("[role='article']").remove();
+      $('span:contains("Birthday")').closest('._4ikz');
+    }
+    down[e.keyCode] = false;
+});
