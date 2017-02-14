930647092076a67b8dab8d04221923c51e52faaf
dddiehn <dddiehn@gmail.com>
2017-02-13 20:38:28 -0600

    delete count and cleanup

--- fboptimitzer.js
+++ fboptimitzer.js
@@ -1,24 +1,22 @@
-// $(document).keypress(function(e) {
-//   if(e.which == 13) {
-//
-//   }
-// });
-
 // http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-in-jquery/10655316#10655316
 var down = [];
 $(document).keydown(function(e) {
     down[e.keyCode] = true;
 }).keyup(function(e) {
+    // 13 is enter
+    // 17 is ctrl
     if (down[17] && down[13]) {
-      console.log('BAHLEETED');
+      count = 0;
 
       // these ones work:
+      count += $('[role="article"] a:contains("ClickHole")').length;
       $('a:contains("ClickHole")').closest("[role='article']").remove();
-      $('span._m8d:contains("Suggested Post")')
+      // well...I thought these were working:
+      $('span._m8d:contains("Suggested Post")');
 
-      // these ones don't work
+      // these ones don't work:
       $('span:contains("Birthday")').closest('._4ikz');
-
+      console.log(count + ' things BAHLEETED');
     }
     down[e.keyCode] = false;
 });
