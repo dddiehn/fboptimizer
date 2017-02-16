c930b51eaea67d74769743e1401a63ba7e171799
dddiehn <dddiehn@gmail.com>
2017-02-15 19:14:08 -0600

    made the html thingy

--- fboptimitzer.js
+++ fboptimitzer.js
@@ -1,4 +1,22 @@
+// http://stackoverflow.com/questions/20849496/using-facebook-sdk-with-chrome-extensions
 // http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-in-jquery/10655316#10655316
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'your-app-id',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 var down = [];
 $(document).keydown(function(e) {
     down[e.keyCode] = true;
