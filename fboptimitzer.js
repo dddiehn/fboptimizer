b37ee52a7de37ac7ebc71ddd54a3a934e9f11aae
dddiehn <dddiehn@gmail.com>
2017-02-21 22:22:53 -0600

    some cleanup because josh might look at it

--- fboptimitzer.js
+++ fboptimitzer.js
@@ -8,24 +8,20 @@ $.expr[":"].contains = $.expr.createPseudo(function(arg) {
 function deleteEm(){
   count = 0;
 
-  // leaving out count information till I think of a cleaner way to do it
-  // count += $('[role="article"] a:contains("ClickHole")').length;
-
   // these ones work:
   // bads stores every string that you don't wanna see in a post
   var politics = ["trump", "democrat", "republican"];
   var clickbait = ["you won't believe"];
-  var lame = ["memes", "birthday", "trash dove", "that moment", "awkward moment", "my face", "my reaction"];
-  var ads = ["suggested page", "tour date"];
+  var lame = ["memes", "birthday", "trash dove", "that moment", "awkward moment", "my face", "my reaction", "in a relationship"];
+  var ads = ["suggested page", "tour date", "sponsered"];
+  var sports = ["football", "nfl", "basketball", "baseball"]
 
   // wow js is lame
-  var bads = politics.concat(clickbait.concat(lame.concat(ads)));
+  // yes, I'm gonna be doing some conditionalizing when I get the config stuff working
+  var bads = politics.concat(clickbait.concat(lame.concat(ads.concat(sports))));
   for (var i = 0; i < bads.length; i++) {
     console.log(bads[i]);
-    deleteSelector = ":contains(\"" + bads[i] + "\")";
-    $("[role='article']" + deleteSelector).closest('._4ikz').remove();
-    // $('p' + deleteSelector).closest("[role='article']").remove();
-    // $('a' + deleteSelector).closest("[role='article']").remove();
+    $("[role='article']:contains(\"" + bads[i] + "\")").closest('._4ikz').remove();
   }
   $('span:contains("\'s Birthday")').closest('._4ikz').remove();
   $('span._m8d:contains("Suggested Post")').closest("[role='article']").remove();
@@ -34,8 +30,7 @@ function deleteEm(){
 }
 
 // http://stackoverflow.com/questions/20849496/using-facebook-sdk-with-chrome-extensions
-
-// http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-in-jquery/10655316#10655316
+// I'm not doing anything with this yet
 window.fbAsyncInit = function() {
     FB.init({
       appId      : 'your-app-id',
@@ -45,7 +40,8 @@ window.fbAsyncInit = function() {
     FB.AppEvents.logPageView();
   };
 
-  (function(d, s, id){
+// http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-in-jquery/10655316#10655316
+(function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
@@ -62,20 +58,18 @@ $(document).keydown(function(e) {
     if (down[17] && down[13]) {
 
       deleteEm();
-      // chrome.storage.sync.set({'value': theValue}, function() {
-      //     // Notify that we saved.
-      //     message('Settings saved');
-      //   });
-
-      // ***************************************************************************************
-      // these ones don't work:
-
-      // special fancy informations
 
     }
     down[e.keyCode] = false;
 });
 
-$("[id^=topnews_main_stream_]").on("change", function(){console.log("taco bell");});
+$("[id^=topnews_main_stream_]").on("change", function(){
+  console.log("taco bell");
+});
 
 deleteEm();
+
+// chrome.storage.sync.set({'value': theValue}, function() {
+//     // Notify that we saved.
+//     message('Settings saved');
+//   });
