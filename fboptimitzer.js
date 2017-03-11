383c912090ead8dd7358a2a4eb4e398c369e9640
dddiehn <dddiehn@gmail.com>
2017-03-10 19:22:06 -0600

    deletecount now a function

--- fboptimitzer.js
+++ fboptimitzer.js
@@ -11,6 +11,9 @@ $.extend($.expr[":"], {
   }
 });
 
+function deleteCount(){
+  return $("[role='article']:hidden").length;
+}
 
 function deleteEm(){
   count = 0;
@@ -18,7 +21,7 @@ function deleteEm(){
   // these ones work:
   // bads stores every string that you don't wanna see in a post
   var politics  = ["trump", "democrat", "republican", "right wing", "left wing", "liberal", "conservative"];
-  var clickbait = ["you won't believe", "faith in humanity", "one weird trick", "buzzfeed"];
+  var clickbait = ["you won't believe", "faith in humanity", "one weird trick", "buzzfeed", "get your tissues ready"];
   var lame      = ["meme", "birthday", "trash dove", "that moment", "awkward moment", "my face", "my reaction", "in a relationship", "recipe", "am i the only one that", "viral", "me:", "smash mouth", "mondays", "leaving work on a friday", "asf", "squad goals", "relationship goals", "fleek", "like if", "share if"];
   var ads       = ["suggested page", "tour date", "sponsered"];
   var sports    = ["football", "nfl", "basketball", "baseball", "nhl", "hockey", "nba"];
@@ -47,8 +50,7 @@ function deleteEm(){
   $('span:contains("\'s Birthday")').closest('._4ikz').hide();
   $('span._m8d:contains("Suggested Post")').closest("[role='article']").hide();
 
-  var deletedCount = $("[role='article']:hidden").length;
-  console.log('BAHLEETED ' + deletedCount + ' sucky posts');
+  console.log('BAHLEETED ' + deleteCount() + ' sucky posts');
 
 }
 // http://stackoverflow.com/questions/20849496/using-facebook-sdk-with-chrome-extensions
@@ -96,7 +98,7 @@ setInterval(function(){
     deleteEm();
     articleCount = newArticleCount
   }else{
-    console.log("It looks clear.");
+    console.log("It looks clear. (" + deleteCount() + " so far)");
   }
 }, 5000);
 
